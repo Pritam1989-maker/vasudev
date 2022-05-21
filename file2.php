c1d2bb528b6d9f977a228fc30874df65508a42f3
Pritam1989-maker <tamang89.pritam@gmail.com>
2022-05-21 18:11:18 +0530

    some modification done using nano

--- file2.php
+++ file2.php
@@ -1,4 +1,6 @@
-this is test file
-again testing data 
-this is testing at 17:43 on file2.php
-hello world
+#this is test file
+#again testing data 
+#this is testing at 17:43 on file2.php
+#hello world
+
+this file is now modifed using nano command
